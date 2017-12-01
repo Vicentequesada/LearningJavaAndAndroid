6293f455f18c8c31b213d0ae199069f7fc226f86
Vicente Quesada Camón <vicentequesadacamon@gmail.com>
2017-12-01 09:21:51 +0100

    Update HiLo.java

--- HiLo.java
+++ HiLo.java
@@ -8,7 +8,7 @@ public class HiLo {
 		do {
 			// Create a random number for the user to guess
 			int theNumber = (int)(Math.random() * 100 + 1);
-			//System.out.println(theNumber);	//Uncoment this in case we want to know the number (for testing).
+			System.out.println(theNumber);		//Uncoment this in case we want to know the number (for testing).
 			int guess = 0;	//Number entered by the player
 			int count = 0;	//Number of tries of guessing the number
 			while (guess != theNumber) {
@@ -21,18 +21,23 @@ public class HiLo {
 					if (guess < theNumber) { // If number entered is smaller
 						System.out.println("The number is bigger" + ", try again!");
 						System.out.println("Number of tries: " + count);
-					} else if (guess > theNumber) { // If number entered is bigger
+					} else if (guess > theNumber) { // If number entered is
+						// bigger
 						System.out.println("The number is smaller" + ", try again!");
 						System.out.println("Number of tries: " + count);
 					} else { // If both previous cases are false
 						System.out.println("Congratulations! You've found the number!");
+
 					}
 				} catch (Exception e) {		//Handle the exception (if the entrance is not a number)
 					System.out.println("Incorrect entering! Please enter a number between 1 and 100.");
-					scan = new Scanner(System.in);	//Create a new object so program doesn't stops when exception
+					scan = new Scanner(System.in);	//Creates another scanner object
 				}
 			}
-		}while(playAgain.equalsIgnoreCase("y"));	//If player enters y, start again.
+			scan = new Scanner(System.in);	//Creates another scanner object
+			System.out.println("Play again? (y/n)");
+			playAgain = scan.nextLine();
+		}while(playAgain.equalsIgnoreCase("y"));		//If player enters y, start again.
 		//Otherwise
 		System.out.println("Thank you for playing! Goodbye :)");
 		scan.close();	//Close scanner
