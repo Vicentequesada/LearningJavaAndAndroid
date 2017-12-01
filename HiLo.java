f85fc467b9a11e7119f3c71d9c071b7906105270
Vicente Quesada Camón <vicentequesadacamon@gmail.com>
2017-12-01 18:59:11 +0100

    Update HiLo.java

--- HiLo.java
+++ HiLo.java
@@ -8,21 +8,19 @@ public class HiLo {
 		do {
 			// Create a random number for the user to guess
 			int theNumber = (int)(Math.random() * 100 + 1);
-			System.out.println(theNumber);		//Uncoment this in case we want to know the number (for testing).
+			System.out.println(theNumber);		//Uncomment this in case we want to know the number (for testing).
 			int guess = 0;	//Number entered by the player
 			int count = 0;	//Number of tries of guessing the number
 			while (guess != theNumber) {
 				try {                       
 					System.out.println("Guess a number between 1 and 100:");
-					guess = scan.nextInt(); // Reads the number typed on the
-					// keyboard by the player
+					guess = scan.nextInt(); // Reads the number typed on the keyboard by the player
 					count++; // Plus 1 every time a number is entered
 					System.out.println("You entered " + guess + ".");
 					if (guess < theNumber) { // If number entered is smaller
 						System.out.println("The number is bigger" + ", try again!");
 						System.out.println("Number of tries: " + count);
-					} else if (guess > theNumber) { // If number entered is
-						// bigger
+					} else if (guess > theNumber) { // If number entered is bigger
 						System.out.println("The number is smaller" + ", try again!");
 						System.out.println("Number of tries: " + count);
 					} else { // If both previous cases are false
